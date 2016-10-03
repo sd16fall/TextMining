138cedd422532be8179cc326a51b3dd07e4f4165
rerafyaw <anderson_nov@hotmail.com>
2016-10-03 14:01:36 -0400

    Offloaded CSV files for data dump and read

--- tweetHarvest.py
+++ tweetHarvest.py
@@ -30,7 +30,7 @@ except:
     ("modality",STRING)])
     index = set()
 
-#license=('ZlvGyUbFwvh6huUZ02VCbG6JP','q9vhGij4NJOLWyKpQsFch5cSPnaATcY2AfrS6SSpaP901seIMk'
+
 # Declare Twitter object, to search for precise stream-based information
 twitter = Twitter(license=None, throttle = 0.5, language='en')
 
