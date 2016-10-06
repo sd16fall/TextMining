40f32f9764e620260498fb6797e85e76427c1327
rerafyaw <anderson_nov@hotmail.com>
2016-10-06 15:49:32 -0400

    Patched to v1.0b (stable) - now includes MATPLOT libraries, refined functional design

--- tweetHarvest.py
+++ tweetHarvest.py
@@ -14,6 +14,9 @@ import re # the regex library for processing sentences
 import datetime # library for system datetime
 import os, sys; sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 import time #for sleep function
+# brand new - patch 1.0b - matplot display
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Opens a CSV(comma-separated value) file for identifying and storing unique tweets
 try:
@@ -49,9 +52,6 @@ def enrolTwitter(thread,location):
         print "Iteration Number", (j+1) #for humans to understand lulz
         print
         for tweet in twitter.search(thread, geo=geocode(location)[:2],start=i,count=5, cached=False):
-            #print tweet.text
-            #print tweet.id
-            #print
             # adds tweet if its ID doesn't exist previously
             if len(table) == 0 or tweet.id not in index:
                 # a series of sentence level filters designed for Twitter handles
@@ -123,6 +123,29 @@ def localSentiment(location,topic):
     avgMode = modeVal/y
     return avgPol, avgSub, avgMode
 
+def globalSentiment(topic):
+    # takes a global average for comparison
+    # recycleable variables - must be reinitialized every time function is called
+    globalPolar = 0.0
+    globalSub = 0.0
+    globalMode = 0.0
+    x = 0.0
+    y = 0.0
+    # opening location-based values in sentiment and modality for averaging
+    for x in range(len(table)):
+        if ((table[x,6] == topic)):
+            y+=1 # for averaging
+            globalPolar += float(table[x,3])
+            globalSub += float(table[x,4])
+            globalMode += float(table[x,5])
+    # average values
+    print "x value is:", (x+1)
+    print "y val is:", y
+    avgPol2 = globalPolar/y
+    avgSub2 = globalSub/y
+    avgMode2 = globalMode/y
+    return avgPol2, avgSub2, avgMode2
+
 # Human intepreter for the numerical data acquired
 def forecast(topic,location1,location2,avgPol1,avgSub1,avgMode1,avgPol2,avgSub2,avgMode2):
     print
@@ -156,36 +179,87 @@ def forecast(topic,location1,location2,avgPol1,avgSub1,avgMode1,avgPol2,avgSub2,
     print
     print "Index divergence between the polarities: ", divergence
 
+def userInterface():
+    # text formatting block, purely for aesthetics, now funtionalized
+    print "Here are a list of cities to choose from (for sample purposes)"
+    print " 1. Singapore \n 2. Brussels \n 3. London \n 4. Paris \n 5. Beijing \n 6. Pretoria \n 7. Tokyo\n 8. Moscow\n"
+    location = raw_input("Please enter a city name: (e.g. Singapore):")
+    # checks if the geocode specified is valid
+    while(verifyGeocode(location) is not True):
+        print "Please try again!"
+        location = raw_input("Please enter a city name: (e.g. Singapore):")
+    print "You cleared the location test!"
+    return location
+
+def plotting(topic,location1,location2,avgPol1,avgSub1,avgMode1,avgPol2,avgSub2,avgMode2,globalPolar,globalSub,globalMode):
+    # number of charts
+    n_groups = 3
+
+    loc1 = (avgPol1,avgSub1,avgMode1)
+    std_1 = (1,2,3)
+
+    loc2 = (avgPol2,avgSub2,avgMode2)
+    std_2 = (1,2,3)
+
+    loc3 = (globalPolar,globalSub,globalMode)
+    std_3 = (1,2,3)
+
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    # thickness of bar plot
+    bar_width = 0.3
+
+    opacity = 0.4
+    error_config = {'ecolor': '0.3'}
+
+    rects1 = plt.bar(index, loc1, bar_width,
+                    alpha=opacity,
+                    color='b',
+                    yerr=std_1,
+                    error_kw=error_config,
+                    label=location1)
+
+    rects2 = plt.bar(index + bar_width, loc2, bar_width,
+                    alpha=opacity,
+                    color='r',
+                    yerr=std_2,
+                    error_kw=error_config,
+                    label=location2)
+
+    rects3 = plt.bar(index + (2*bar_width), loc3, bar_width,
+                    alpha=opacity,
+                    color='y',
+                    yerr=std_3,
+                    error_kw=error_config,
+                    label='Average')
+
+
+    plt.xlabel('Geolocation')
+    plt.ylabel('Value')
+    plt.title('Sentient analysis by geocodes')
+    plt.xticks(index + bar_width, ('Polarity', 'Subjectivity', 'Modality'))
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 # Main program block
 if __name__ == "__main__":
-    # text formatting block, purely for aesthetics
-    print "=========== SENTIMENT ANALYSIS TOOL v0.2b =========="
+    print "=========== SENTIMENT ANALYSIS TOOL v1.0b =========="
     print
     print "Returns statistics on the variance of opinions with regards to a topic;"
     print "This program requires (2) geolocations, restricted to capitals, and (1) topic of interest"
     print
-    print "Here are a list of cities to choose from (for sample purposes)"
-    print " 1. Singapore \n 2. Brussels \n 3. London \n 4. Paris \n 5. Beijing \n 6. Pretoria \n 7. Tokyo\n 8. Moscow\n"
-    location1 = raw_input("Please enter a city name: (e.g. Singapore):")
-    while(verifyGeocode(location1) is not True):
-        print "Please try again!"
-        location1 = raw_input("Please enter a city name: (e.g. Singapore):")
-    print "You cleared the location test!"
-
-    # Phase II: Enrol topic and geocode (not gonna functionalize for time's sake)
+    # Phase I : Provides user guidance and takes first geolocation
+    location1 = userInterface()
+    # Phase II: Enrols topic and 2nd geocode
     topic = raw_input("Enter a phrase, word, question you would like to analyze:")
     enrolTwitter(topic,location1)
     print "Individual sentiment and modality committed."
     print
     print "Please prepare to enrol the second city:"
     print
-    print "Here are a list of cities to choose from (for sample purposes)"
-    print " 1. Singapore \n 2. Brussels \n 3. London \n 4. Paris \n 5. Beijing \n 6. Pretoria \n 7. Tokyo\n 8. Moscow\n"
-    location2 = raw_input("Please enter a city name: (e.g. Paris):")
-    while(verifyGeocode(location2) is not True):
-        print "Please try again!"
-        location2 = raw_input("Please enter a city name: (e.g. Singapore):")
-    print "You cleared the location test!"
+    location2 = userInterface()
     enrolTwitter(topic,location2)
     print "Individual sentiment and modality committed."
     print
@@ -195,11 +269,22 @@ if __name__ == "__main__":
     avgPol1, avgSub1, avgMode1 = localSentiment(location1,topic)
     print
     print location1, "statistics-\n Polarity:", avgPol1,"\n Subjectivity: ", avgSub1,"\n Modality:", avgMode1
+    print
     avgPol2, avgSub2, avgMode2 = localSentiment(location2,topic)
     print
     print location2, "statistics-\n Polarity:", avgPol2,"\n Subjectivity: ", avgSub2,"\n Modality:", avgMode2
     print
+    globalPol, globalSub, globalMode = globalSentiment(topic)
+    print
+    print "Global statistics -\n Polarity:", globalPol,"\n Subjectivity: ", globalSub,"\n Modality:", globalMode
+    print
     time.sleep(1)
     # explanatory function to communicate the idea behind the statistics acquired
     forecast(topic,location1,location2,avgPol1,avgSub1,avgMode1,avgPol2,avgSub2,avgMode2)
+    # PLUS : explanatory graph to communicate the scale of opinion
+    print
+    print "Creating plots and subplots.."
+    print
+    time.sleep(2)
+    plotting(topic,location1,location2,avgPol1,avgSub1,avgMode1,avgPol2,avgSub2,avgMode2,globalPol,globalSub,globalMode)
     cache.clear() # empties the local search bin before terminating
